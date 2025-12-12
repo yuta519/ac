bb739c828f7e6ad57b36dd4a3d1a370080d54fef
yuta519 <ytkwmr18@icloud.com>
2025-12-11 22:29:56 -0500

    feat:ABC042-C

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,18 @@
 module Main (main) where
 
-import ABC042 (solveA, solveB)
-import IOUtils (getLineXTimes)
+import ABC042 (solveC)
+
+-- import IOUtils (getLineXTimes)
+--
 
 main :: IO ()
 main = do
-  input <- getLine
-  let n = case words input of
-        (x : _) -> x
-        [] -> "0"
-  inputs <- getLineXTimes $ read n
-  putStrLn $ solveB inputs
+  inputN <- getLine
+  let (n, _) = case words inputN of
+        (x : y : _) -> (x, y)
+        (x : []) -> (x, "0")
+        [] -> ("0", "0")
+  inputDs <- getLine
+  let dislikes = [read d :: Int | d <- words inputDs]
+
+  print $ solveC dislikes (read n :: Int)
--- lib/ABC042.hs
+++ lib/ABC042.hs
@@ -1,13 +1,15 @@
-module ABC042 (solveA, solveB) where
+module ABC042 (solveA, solveB, solveC) where
 
 import Data.List (sort)
 import Data.Maybe (catMaybes)
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Text.Read (readMaybe)
 
 solveA :: String -> String
 solveA syllables = if count 5 syls == 2 && count 7 syls == 1 then "YES" else "NO"
- where
- syls = stringsToInts [s | s <- words syllables]
+  where
+    syls = stringsToInts [s | s <- words syllables]
 
 count :: Int -> [Int] -> Int
 count x xs = length $ filter (== x) xs
@@ -17,3 +19,12 @@ stringsToInts xs = catMaybes $ map readMaybe xs
 
 solveB :: [String] -> String
 solveB xs = concat $ sort xs
+
+digits :: Int -> [Int]
+digits n = map (read . (: [])) (show n)
+
+outOfObsession :: Set Int -> Int -> Bool
+outOfObsession dislikes n = all (not . (`Set.member` dislikes)) $ digits n
+
+solveC :: [Int] -> Int -> Int
+solveC dislikes n = head [m | m <- [n ..], outOfObsession (Set.fromList dislikes) m]
