2db031d687355a3ebba389579e56520b1b4fda0f
yuta519 <ytkwmr18@icloud.com>
2025-12-27 08:37:58 -0500

    feat: advent of code 2025 Day1 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,9 @@
 module Main (main) where
 
-import ABC042 (solveC)
-
--- import IOUtils (getLineXTimes)
---
+import AdventOfCode2025Day1 (part1)
+import IOUtils (readMultiLinesFile)
 
 main :: IO ()
 main = do
-  inputN <- getLine
-  let (n, _) = case words inputN of
-        (x : y : _) -> (x, y)
-        (x : []) -> (x, "0")
-        [] -> ("0", "0")
-  inputDs <- getLine
-  let dislikes = [read d :: Int | d <- words inputDs]
-
-  print $ solveC dislikes (read n :: Int)
+  inputs <- readMultiLinesFile "inputs/adventofcode-2025-day.txt"
+  print $ part1 inputs
