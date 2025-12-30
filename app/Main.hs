fbcf01e23f808e50cca1e24f2e2e584041dcde3e
yuta519 <ytkwmr18@icloud.com>
2025-12-30 06:57:02 -0500

    feat: ABC044-A

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
-import ABC043
-
--- import IOUtils
+import ABC044
+import IOUtils
 
 main :: IO ()
 main = do
-  s <- getLine
-  let (start, end) = solveD s
-  putStrLn $ show start ++ " " ++ show end
+  n <- getInt
+  k <- getInt
+  x <- getInt
+  y <- getInt
+
+  print $ solveA n k x y
--- lib/ABC044.hs
+++ lib/ABC044.hs
@@ -1,4 +1,6 @@
 module ABC044 where
 
-solveA :: Int -> Int
-solveA a = 0
+solveA :: Int -> Int -> Int -> Int -> Int
+solveA n k x y
+  | n <= k = n * x
+  | otherwise = k * x + (n - k) * y
