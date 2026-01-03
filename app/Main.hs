43105ad32d78d182390e950960b386c5458e2281
yuta519 <ytkwmr18@icloud.com>
2026-01-03 07:58:23 -0500

    feat: ABC163-A not AC
    My original answer which didn't get AC

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main (main) where
 
-import ABC061
+import ABC163
 import IOUtils
 
 main :: IO ()
 main = do
-  [n, m] <- getInts
-  roads <- getLineXTimes m
-  mapM_ print $ solveB n [(a, b) | road <- roads, let [a, b] = fmap (read :: String -> Int) $ words road]
+  n <- getInt
+  as <- getInts
+  mapM_ print $ solveC n as
--- lib/ABC163.hs
+++ lib/ABC163.hs
@@ -1,1 +1,10 @@
 module ABC163 where
+
+solveC :: Int -> [Int] -> [Int]
+solveC 0 [] = []
+solveC n as = do
+  let xs = [0 | _ <- [1 .. n]] in incAt as xs
+
+incAt :: [Int] -> [Int] -> [Int]
+incAt (a : []) xs = take (a - 1) xs ++ [xs !! (a - 1) + 1] ++ drop a xs
+incAt (a : as) xs = incAt as (take (a - 1) xs ++ [xs !! (a - 1) + 1] ++ drop a xs)
