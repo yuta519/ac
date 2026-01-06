99d25029113079fd01d88546bd3ab6d2909c0a89
Yuta Kawamura <ytkwmr18@icloud.com>
2026-01-06 04:16:51 -0800

    feat: ABC068-C still TLE

--- lib/ABC068.hs
+++ lib/ABC068.hs
@@ -1,11 +1,20 @@
 module ABC068 where
 
+import Data.Array
+import qualified Data.IntSet as S
+
 solveC :: Int -> [(Int, Int)] -> Bool
 solveC 0 [] = False
 solveC n boats =
-  let fromFirst = neighboarsOfX boats 1
-      fromN = neighboarsOfX boats n
-   in any (`elem` fromN) fromFirst
+  let adj =
+        accumArray
+          (++)
+          []
+          (1, n)
+          ([(a, [b]) | (a, b) <- boats] ++ [(b, [a]) | (a, b) <- boats])
+      fromFirst = adj ! 1
+      fromN = S.fromList (adj ! n)
+   in any (`S.member` fromN) fromFirst
 
 neighboarsOfX :: [(Int, Int)] -> Int -> [Int]
 neighboarsOfX edges x = concatMap (\(a, b) -> [b | a == x] ++ [a | b == x]) edges
