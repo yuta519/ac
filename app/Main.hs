be3ccc5d5982461886e68570a34f5db8deacf2b2
yuta519 <ytkwmr18@icloud.com>
2026-01-28 06:49:16 -0500

    feat: ABC083-A

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,9 @@
 module Main (main) where
 
-import ABC081
+import ABC083
 import IOUtils
 
 main :: IO ()
 main = do
-  [_n, k] <- getInts
-  as <- getInts
-  print $ solveC k as
+  [a, b, c, d] <- getInts
+  putStrLn $ solveA (a, b, c, d)
--- lib/ABC083.hs
+++ lib/ABC083.hs
@@ -1,5 +1,11 @@
 module ABC083 where
 
+solveA :: (Int, Int, Int, Int) -> String
+solveA (a, b, c, d)
+  | a + b > c + d = "Left"
+  | a + b < c + d = "Right"
+  | a + b == c + d = "Balanced"
+
 solveB :: Int -> Int -> Int -> Int
 solveB n a b = sum [i | i <- [a .. n], (base10 i) >= a && (base10 i) <= b]
 
