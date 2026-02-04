252fe27adfa562c45769452120bf545683d8b3a4
yuta519 <ytkwmr18@icloud.com>
2026-02-04 06:48:21 -0500

    feat: ABC065-B

--- lib/ABC065.hs
+++ lib/ABC065.hs
@@ -1,5 +1,6 @@
 module ABC065 where
 
+import qualified Data.Array.Unboxed as U
 import Data.Set
 
 -- 3 1 2
@@ -9,7 +10,9 @@ import Data.Set
 solveB :: [Int] -> Int
 solveB as = go 1 0 empty
   where
+    arr :: U.UArray Int Int
+    arr = U.listArray (1, (length as)) as
     go cur steps visited
       | cur == 2 = steps
       | member cur visited = -1
-      | otherwise = go (as !! (cur - 1)) (steps + 1) (insert cur visited)
+      | otherwise = go (arr U.! cur) (steps + 1) (insert cur visited)
