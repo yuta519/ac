04d7d92700764d6ad3064cae3b42f6f91de7e1f6
yuta519 <ytkwmr18@icloud.com>
2026-02-08 22:11:44 -0500

    feat: ABC053-A

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,10 @@
 module Main (main) where
 
-import ABC088
+import ABC053
 import IOUtils
 
 main :: IO ()
 main = do
   x <- getInt
-  a <- getInt
 
-  putStrLn $ if solveA x a then "Yes" else "No"
+  putStrLn $ solveA x
---
+++ lib/ABC053.hs
@@ -0,0 +1,4 @@
+module ABC053 where
+
+solveA :: Int -> String
+solveA x = if x < 1200 then "ABC" else "ARC"
