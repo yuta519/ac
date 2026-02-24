986d7d818936d8b858845574fb5efb5b720e546a
yuta519 <ytkwmr18@icloud.com>
2026-02-24 06:43:01 -0500

    feat: AGC010-A'

--- lib/AGC010.hs
+++ lib/AGC010.hs
@@ -4,3 +4,6 @@ solveA :: [Int] -> String
 solveA as =
   let oddLength = length [a | a <- as, a `mod` 2 == 1]
    in if oddLength `mod` 2 == 0 then "YES" else "NO"
+
+-- let m = (length . filter odd) as
+-- in if even m then "YES" else "NO"
