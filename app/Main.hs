66eaef8d33be8b8a5d05a83938fc4d4ffff8eef5
yuta519 <ytkwmr18@icloud.com>
2026-03-02 20:17:04 -0500

    feat: ABC175-A

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import AGC025
+import ABC175
 
 -- import IOUtils
 
--- lib/ABC175.hs
+++ lib/ABC175.hs
@@ -1,5 +1,14 @@
 module ABC175 where
 
+import Data.List (foldl')
+
+solveA :: String -> Int
+solveA s = snd $ foldl' go (0, 0) s
+  where
+    go (cur, ans) c
+      | c == 'R' = (cur + 1, max (cur + 1) ans)
+      | c == 'S' = (0, ans)
+
 solveC :: Integer -> Integer -> Integer -> Integer
 solveC x k d
   | k <= (x' `div` d) = x' - k * d
