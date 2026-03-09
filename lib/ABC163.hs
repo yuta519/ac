f3a7e5ebfb564bb6ee285cef451a48dec2980919
yuta519 <ytkwmr18@icloud.com>
2026-03-09 06:46:12 -0400

    chore

--- lib/ABC163.hs
+++ lib/ABC163.hs
@@ -6,9 +6,7 @@ solveA :: Float -> Float
 solveA r = 2.0 * r * 3.14
 
 solveB :: Int -> [Int] -> Int
-solveB n as
-  | n >= total_homework = n - total_homework
-  | otherwise = -1
+solveB n as = if n >= total_homework then n - total_homework else -1
   where
     total_homework = sum as
 
