99d410420d249ffea5b1fcd6922894acf2a4b497
yuta519 <ytkwmr18@icloud.com>
2026-03-20 19:35:18 -0400

    chore

--- devenv.nix
+++ devenv.nix
@@ -4,18 +4,15 @@
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
   packages = with pkgs; [ 
     git 
 
-    # Toolchain
     ghc                       # GHC compiler
     # Haskell toolchain (custom GHC with packages)
     (pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
       containers      # <- you need this for Data.Set
       text            # commonly needed
       bytestring      # commonly needed
-      # add more if needed
     ]))
     cabal-install             # Cabal
     haskell-language-server   # HLS (the LSP server)
@@ -31,9 +28,6 @@
   languages.haskell.enable = true;
   languages.haskell.package = pkgs.haskell.compiler.ghc96;
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
   # https://devenv.sh/scripts/
   scripts.hello.exec = ''
     echo hello from $GREET
@@ -45,20 +39,12 @@
     git --version # Use packages
   '';
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
 
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
