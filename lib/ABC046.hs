77a059a91099d221be1da2c62841465fd791714d
yuta519 <ytkwmr18@icloud.com>
2026-04-03 21:25:30 -0400

    feat: ABC046-C

--- lib/ABC046.hs
+++ lib/ABC046.hs
@@ -9,19 +9,13 @@ solveA a b c = length $ Set.fromList [a, b, c]
 solveB :: Int -> Int -> Int
 solveB n k = k * (k - 1) ^ (n - 1)
 
-solveC :: [(Int, Int)] -> (Int, Int)
-solveC elects = foldl' go (1, 1) elects
+solveC :: [(Int, Int)] -> Int
+solveC [] = 0
+solveC (first : rest) = uncurry (+) $ foldl' go first rest
   where
-    go (baseT, baseA) (curT, curA)
-      | baseT < curT && baseA < curA = (curT, curA)
-      | baseT > curT && baseA < curA =
-          let x = if baseT `mod` curT == 0 then baseT `div` curT else baseT `div` curT + 1
-           in (x * curT, x * curA)
-      | baseT < curT && baseA > curA =
-          let x = if baseA `mod` curA == 0 then baseA `div` curA else baseA `div` curA + 1
-           in (x * curT, x * curA)
-      | baseT > curT && baseA > curA =
-          let base = if baseT > baseA then baseT else baseA
-              cur = if baseT > baseA then curT else curA
-              x = if base `mod` cur == 0 then base `div` cur else base `div` cur + 1
-           in (x * curT, x * curA)
+    go (baseT, baseA) (curT, curA) =
+      let kT = ceil baseT curT
+          kA = ceil baseA curA
+          k = max kT kA
+       in (k * curT, k * curA)
+    ceil x y = (x + y - 1) `div` y
