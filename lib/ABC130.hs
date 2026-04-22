fa8606baccbf569f84914f490ab74f96f4734bcd
yuta519 <ytkwmr18@icloud.com>
2026-04-21 20:31:20 -0400

    feat: ABC130-B with "takeWhile"

--- lib/ABC130.hs
+++ lib/ABC130.hs
@@ -6,6 +6,6 @@ solveA :: Int -> Int -> Int
 solveA x a = if x < a then 0 else 10
 
 solveB :: Int -> [Int] -> Int
-solveB x ls = sum [if p <= x then 1 else 0 | p <- points]
+solveB x ls = length $ takeWhile (<= x) points
   where
     points = scanl' (+) 0 ls
