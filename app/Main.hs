a8c3529e4d57d2492c97643db2edb2a2036e7f82
yuta519 <ytkwmr18@icloud.com>
2026-04-26 09:43:13 -0400

    feat: ABC130-C

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,10 @@ module Main (main) where
 
 import ABC130
 import IOUtils
+import Text.Printf (printf)
 
 main :: IO ()
 main = do
-  [_, x] <- getInts
-  ls <- getInts
-
-  print $ solveB x ls
+  [w, h, x, y] <- getInts
+  let (a, b) = solveC w h x y
+  printf "%.6f %d\n" a b
--- lib/ABC130.hs
+++ lib/ABC130.hs
@@ -9,3 +9,8 @@ solveB :: Int -> [Int] -> Int
 solveB x ls = length $ takeWhile (<= x) points
   where
     points = scanl' (+) 0 ls
+
+solveC :: Int -> Int -> Int -> Int -> (Double, Int)
+solveC w h x y
+  | w == 2 * x && h == 2 * y = (fromIntegral (w * h) / 2, 1)
+  | otherwise = (fromIntegral (w * h) / 2, 0)
