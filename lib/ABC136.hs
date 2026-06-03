d142491174c26e68ac781afc016056b8124cf879
yuta519 <ytkwmr18@icloud.com>
2026-06-03 15:03:52 -0400

    feat: ABC136-B

--- lib/ABC136.hs
+++ lib/ABC136.hs
@@ -4,3 +4,6 @@ solveA :: Int -> Int -> Int -> Int
 solveA a b c =
   let remain = a - b
    in if c <= remain then 0 else c - remain
+
+solveB :: Int -> Int
+solveB n = length [i | i <- [1 .. n], odd $ length (show i)]
