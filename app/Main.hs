0f2f1438ffcf3c574b8c1da1d837a15a710d4530
yuta519 <ytkwmr18@icloud.com>
2026-06-26 09:10:50 -0400

    feat: ABC139-B

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,10 @@
 module Main (main) where
 
-import ATC001
-import Data.List (elemIndex)
-import Data.Maybe (mapMaybe)
+import ABC139
 import IOUtils
 
 main :: IO ()
 main = do
-  [h, _] <- getInts
-  grid <- getLineXTimes h
-  let start = findStart grid
+  [a, b] <- getInts
 
-  putStrLn $ if solveA start grid then "Yes" else "No"
-
-findStart :: [String] -> (Int, Int)
-findStart grid = head $ mapMaybe findStartInRow (zip [0 ..] grid)
-  where
-    findStartInRow :: (Int, String) -> Maybe (Int, Int)
-    findStartInRow (x, row) = do
-      y <- elemIndex 's' row
-      pure (x, y)
+  print $ solveB a b
--- lib/ABC139.hs
+++ lib/ABC139.hs
@@ -2,3 +2,10 @@ module ABC139 where
 
 solveA :: String -> String -> Int
 solveA s t = sum [1 | i <- [0 .. 2], s !! i == t !! i]
+
+solveB :: Int -> Int -> Int
+solveB a b = go 0
+  where
+    go cur
+      | 1 + (a - 1) * cur < b = go (cur + 1)
+      | otherwise = cur
