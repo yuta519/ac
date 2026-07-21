10cf2f843adef8e66a1dcaf937381434f11fc6f0
yuta519 <ytkwmr18@icloud.com>
2026-07-20 22:02:26 -0400

    chore

--- lib/ABC142.hs
+++ lib/ABC142.hs
@@ -8,8 +8,5 @@ solveA n = fromIntegral ((n + 1) `div` 2) / fromIntegral n
 solveB :: Int -> [Int] -> Int
 solveB k hs = length [h | h <- hs, h >= k]
 
--- 3 [2, 3, 1]
--- 3 [(1, 1), (2, 0), (3, 2)]
--- 3 [(3, 2), (1, 1), (2, 0)]
 solveC :: Int -> [Int] -> [Int]
 solveC n as = map fst $ sortOn snd $ zip [1 .. n] as
