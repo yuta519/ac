cd255cb053de4362c83ebfa06e185f5748ccd251
yuta519 <ytkwmr18@icloud.com>
2026-07-24 16:08:58 -0400

    feat: ABC143-B'

--- lib/ABC143.hs
+++ lib/ABC143.hs
@@ -1,9 +1,11 @@
 module ABC143 where
 
+import Data.List (tails)
+
 solveA :: Int -> Int -> Int
 solveA a b
   | a - b * 2 > 0 = a - b * 2
   | otherwise = 0
 
 solveB :: [Int] -> Int
-solveB ds = sum [(ds !! i) * (ds !! j) | i <- [0 .. (length ds - 2)], j <- [i + 1 .. (length ds - 1)]]
+solveB ds = sum [x * y | (x : ys) <- tails ds, y <- ys]
