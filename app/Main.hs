983ebf4da9f65ee797d0c94e412167fbd798e354
yuta519 <ytkwmr18@icloud.com>
2026-07-29 22:11:17 -0400

    feat: ABC144-B

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,10 @@
 module Main (main) where
 
-import ABC143
+import ABC144
 import IOUtils
 
 main :: IO ()
 main = do
   n <- getInt
-  ls <- getInts
 
-  print $ solveD n ls
+  putStrLn $ solveB n
--- lib/ABC144.hs
+++ lib/ABC144.hs
@@ -4,3 +4,6 @@ solveA :: Int -> Int -> Int
 solveA a b
   | 1 <= a && a <= 9 && 1 <= b && b <= 9 = a * b
   | otherwise = -1
+
+solveB :: Int -> String
+solveB n = if sum [1 | i <- [1 .. 9], n `div` i < 10 && n `mod` i == 0] > 0 then "Yes" else "No"
