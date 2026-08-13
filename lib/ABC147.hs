3e093e777e8936380ab475a71de83aa12fc1fbe6
yuta519 <ytkwmr18@icloud.com>
2026-08-13 08:40:19 -0400

    chore

--- lib/ABC147.hs
+++ lib/ABC147.hs
@@ -4,4 +4,4 @@ solveA :: Int -> Int -> Int -> String
 solveA a b c = if sum [a, b, c] <= 21 then "win" else "bust"
 
 solveB :: String -> Int
-solveB s = sum [if x then 1 else 0 | x <- zipWith (/=) s (reverse s)] `div` 2
+solveB s = length (filter id (zipWith (/=) s (reverse s))) `div` 2
