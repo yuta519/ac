6cbb5933c65d467dedcfe18fbf2e2078fdd17e8b
yuta519 <ytkwmr18@icloud.com>
2026-08-16 21:40:25 -0400

    feat: ABC147-C

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,25 @@
 module Main (main) where
 
-import ABC148
+import ABC147
+import Control.Monad
 import IOUtils
 
 main :: IO ()
 main = do
-  a <- getInt
-  b <- getInt
+  n <- getInt
+  xs <- replicateM n readTestimonies
 
-  print $ solveA a b
+  print $ solveC xs
+
+-- Each person's block is a count followed by that many "x y" lines.
+readTestimonies :: IO [(Int, Int)]
+readTestimonies = do
+  a <- getInt
+  replicateM a readPair
+
+readPair :: IO (Int, Int)
+readPair = do
+  ns <- getInts
+  case ns of
+    [x, y] -> pure (x, y)
+    _ -> error "expected two integers"
--- lib/ABC147.hs
+++ lib/ABC147.hs
@@ -1,7 +1,15 @@
 module ABC147 where
 
+import Control.Monad (replicateM)
+
 solveA :: Int -> Int -> Int -> String
 solveA a b c = if sum [a, b, c] <= 21 then "win" else "bust"
 
 solveB :: String -> Int
 solveB s = length (filter id (zipWith (/=) s (reverse s))) `div` 2
+
+solveC :: [[(Int, Int)]] -> Int
+solveC xs = maximum [length $ filter id c | c <- combinations, consistent c]
+  where
+    combinations = replicateM (length xs) [True, False]
+    consistent c = and [c !! (x - 1) == (y == 1) | (isHonest, sts) <- zip c xs, isHonest, (x, y) <- sts]
